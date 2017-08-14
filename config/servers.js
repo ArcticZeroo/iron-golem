ec1cbc03aa5a2dbd07bb577a9c98fc5d8a844008
Spencer <spencer@artix.cc>
2017-08-14 13:16:54 -0400

    add replyFormatter to config/servers

--- config/servers.js
+++ config/servers.js
@@ -7,51 +7,38 @@ module.exports = [
             {
                 name: 'server-message',
                 regex: /^(.+?)> (.+)$/,
-                matches: ['fullText', 'prefix', 'text'],
-                reply: {
-                    property: ''
-                }
+                matches: ['fullText', 'prefix', 'text']
             },
             {
                 name: 'chat',
                 // What in god's name have I done
                 regex: /^(\d{1,3})\s+(?:([A-Z.]{1,12})\s+)?(\$|[A-Za-z0-9_]{1,16})\s+(.+)$/,
                 matches: ['fullText', 'level', 'rank', 'sender', 'text'],
-                reply: {
-
-                }
+                replyFormatter:  (text, sender) => `${sender}: ${text}`
             },
             {
                 name: 'private-message',
                 regex: /^(\$|[A-Za-z0-9_]{1,16}) > (\$|[A-Za-z0-9_]{1,16}) (.+)$/,
                 matches: ['fullText', 'sender', 'target', 'text'],
-                reply: {
-
-                }
+                replyFormatter:  (text, sender) => `/m ${sender} ${text}`
             },
             {
                 name: 'staff-message-receive',
                 regex: /^<- ([A-Za-z.]{1,16}) (\$|[A-Za-z0-9_]{1,16}) (.+)$/,
                 matches: ['fullText', 'rank', 'sender', 'text'],
-                reply: {
-
-                }
+                replyFormatter:  (text, sender) => `/ma ${sender}: ${text}`
             },
             {
                 name: 'staff-message-send',
                 regex: /^-> ([A-Za-z.]{1,16}) (\$|[A-Za-z0-9_]{1,16}) (.+)$/,
                 matches: ['fullText', 'rank', 'target', 'text'],
-                reply: {
-
-                }
+                replyFormatter:  (text, sender) => `/ma ${sender} ${text}`
             },
             {
                 name: 'staff-chat',
                 regex: /^([A-Za-z.]+) (\$|[A-Za-z0-9_]{1,16}) (.+)$/,
                 matches: ['fullText', 'rank', 'sender', 'text'],
-                reply: {
-
-                }
+                replyFormatter:  (text, sender) => `/a ${sender}: ${text}`
             }
         ]
     }
