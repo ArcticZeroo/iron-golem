6701dec1db7aed48ce0c100dc32e24e646879da8
Spencer <spencer@artix.cc>
2017-08-14 18:38:29 -0400

    Add tests, make MinecraftUtil parse 3D section signs

---
+++ test/test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+
+const ConsoleColor = require('../lib/enum/ConsoleColor');
+const MinecraftUtil = require('../lib/util/MinecraftUtil');
+
+describe('MinecraftUtil', function () {
+    // 2D means §4test instead of §4§ltest, which is 3D
+    it('should be able to convert multi-color 2D section sign messages', function () {
+        const baseMC = '§bHello §cWorld!';
+        const baseChalk = ConsoleColor.Minecraft.AQUA('Hello ') + ConsoleColor.Minecraft.RED('World!');
+
+        assert.equal(MinecraftUtil.textToChalk(baseMC), baseChalk);
+    });
+
+    it('should be able to convert multi-color 3D section sign messages', function () {
+        const baseMC = '§b§lHello World!';
+        const baseChalk = ConsoleColor.Minecraft.BOLD(ConsoleColor.Minecraft.AQUA('Hello World!'));
+
+        assert.equal(MinecraftUtil.textToChalk(baseMC), baseChalk);
+    });
+});
