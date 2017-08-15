119a1f565c5ebb2a40cb03f0f24b9657a1834f4b
Spencer <spencer@artix.cc>
2017-08-14 23:57:33 -0400

    Don't match users w/ all uppercase name in chat

--- config/servers.js
+++ config/servers.js
@@ -1,3 +1,5 @@
+const mineplexRanks = 'ULTRA|HERO|LEGEND|TITAN|ETERNAL|TWITCH|YT|YOUTUBE|TRAINEE|MOD|CMA|SR.MOD|C.MOD|SUPPORT|JR.DEV|ADMIN|DEV|LEADER|OWNER';
+
 module.exports = [
     {
         server: /\.?mineplex\.com$/i,
@@ -12,7 +14,8 @@ module.exports = [
             {
                 name: 'chat',
                 // What in god's name have I done
-                regex: /^(\d{1,3})\s+(?:([A-Z.]{1,12})\s+)?(\$|[A-Za-z0-9_]{1,16})\s+(.+)$/,
+                // This works but captures wrong for uppercase names with no rank: /^(\d{1,3})\s+(?:([A-Z.]{1,12})\s+)?(\$|[A-Za-z0-9_]{1,16})\s+(.+)$/
+                regex: new RegExp(`^(\\d{1,3})\\s+(?:(${mineplexRanks})\\s+)?(\\$|[A-Za-z0-9_]{1,16})\\s+(.+)$`),
                 matches: ['fullText', 'level', 'rank', 'sender', 'text'],
                 replyFormatter:  (text, sender) => `${sender}: ${text}`
             },
